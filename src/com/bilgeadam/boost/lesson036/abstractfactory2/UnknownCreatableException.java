977a7f45754664db1cbb2b99cf5e8be449696afd
Gozde SAYGILI YALCIN <saygiligozde@gmail.com>
2022-07-19 10:54:46 +0300

    "first commit"

---
+++ src/com/bilgeadam/boost/lesson036/abstractfactory2/UnknownCreatableException.java
@@ -0,0 +1,11 @@
+package com.bilgeadam.boost.lesson036.abstractfactory2;
+
+public class UnknownCreatableException extends Exception {
+
+
+	private static final long serialVersionUID = 1L;
+	
+	public UnknownCreatableException(String message) {
+
+	}
+}
